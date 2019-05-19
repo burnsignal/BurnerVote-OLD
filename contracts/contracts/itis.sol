8f589455a231e23043394817f007ad94ed82c0d4
Tomas Vrba <14039565+Tomasvrba@users.noreply.github.com>
2019-05-18 22:36:08 -0400

    proposal-options: emit an event when an option cont. gets money

--- contracts/contracts/itis.sol
+++ contracts/contracts/itis.sol
@@ -5,14 +5,22 @@ contract VoteOption {
     address owner;
     address voteOptionAddress;
     uint deadline;
+    string name;
     string option;
     
-    constructor(uint _deadline, string memory _option) public {
+    constructor(uint _deadline, string memory _name, string memory _option) public {
         owner = msg.sender;
         creator = VoteProposal(msg.sender);
         voteOptionAddress = address(this);
         deadline = _deadline;
+        name = _name;
         option = _option;
+    }
+    
+    event AnonymousDeposit(address indexed from, uint value, string name, string option);
+    
+    function () external payable {
+		emit AnonymousDeposit(msg.sender, msg.value, name, option);
     }
 }
 
@@ -22,10 +30,6 @@ contract VoteProposal {
     uint deadline;
     string name;
     string data;
-    
-    /**
-     * Storage
-     */
     
     mapping(uint => address) public options;
     
@@ -37,12 +41,12 @@ contract VoteProposal {
         data = _data;
     }
     
-    function createOptions(uint _deadline) 
+    function createOptions(uint _deadline, string calldata _name) 
         external
         returns (VoteOption newVoteOptionA, VoteOption newVoteOptionB)
     {
-        VoteOption yes = new VoteOption(_deadline, "yes");
-        VoteOption no = new VoteOption(_deadline, "no"); 
+        VoteOption yes = new VoteOption(_deadline, _name, "yes");
+        VoteOption no = new VoteOption(_deadline, _name, "no"); 
         options[0] = address(yes);
         options[1] = address(no);
         return (yes, no);
@@ -50,11 +54,7 @@ contract VoteProposal {
 }
 
 contract VoteProposalPool {
-    
-    /**
-     * Storage
-     */
-    
+
     function newVoteProposal(
         string calldata _name,
         string calldata _data,
@@ -65,23 +65,32 @@ contract VoteProposalPool {
         returns (VoteProposal newProposal)
     {
         VoteProposal proposal = new VoteProposal(_deadline, _name, _data);
-        proposal.createOptions(_deadline);
-        emit newProposalIssued(msg.sender, _deadline, _name, _data, "yes", proposal.options(0), "no", proposal.options(1));
+        proposal.createOptions(_deadline, _name);
+        emit newProposalIssued(
+            msg.sender, 
+            _deadline, 
+            _name, 
+            _data, 
+            "yes", 
+            proposal.options(0), 
+            "no", 
+            proposal.options(1));
         return (proposal);
     }
     
-    /**
-     * Modifiers
-     */
      
     modifier validateDeadline(uint _newDeadline) {
       require(_newDeadline > now);
       _;
     }
     
-    
-    /**
-     * Events
-     */
-     event newProposalIssued(address issuer, uint deadline, string name, string data, string optionA, address optionAaddr, string optionB, address optionBaddr);
+    event newProposalIssued(
+        address issuer, 
+        uint deadline, 
+        string name, 
+        string data, 
+        string optionA, 
+        address optionAaddr,
+        string optionB,
+        address optionBaddr);
 }
